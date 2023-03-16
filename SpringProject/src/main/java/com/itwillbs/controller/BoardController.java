cac94b1c82c367bf6551948fc7f377043d7be451
user <user@DESKTOP-2PGRMF4>
2023-03-16 14:58:19 +0900

    0316new

--- SpringProject/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringProject/src/main/java/com/itwillbs/controller/BoardController.java
@@ -1,14 +1,19 @@
 package com.itwillbs.controller;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.itwillbs.domain.BoardDTO;
 import com.itwillbs.domain.PageDTO;
@@ -21,6 +26,10 @@ public class BoardController {
 	// 스프링 객체생성 방식 => 의존관계주입(DI : Dependency Injection)
 	@Inject
 	private BoardService boardService;
+	
+	//xml 업로드 경로이름("uploadPath") 자동으로 불러오기
+	@Resource(name = "uploadPath")
+	private String uploadPath;
 	
 	@RequestMapping(value = "/board/write", method = RequestMethod.GET)
 	public String write() {
@@ -137,6 +146,37 @@ public class BoardController {
 		// 주소변경 없이 이동
 		// /WEB-INF/views/board/fwriteForm.jsp
 		return "board/fwriteForm";
+	}
+	
+	@RequestMapping(value = "/board/fwritePro", method = RequestMethod.POST)
+	public String fwritePro(HttpServletRequest request, MultipartFile file) throws Exception{
+		//  throws Exception => 예외처리를 메서드 호출한 곳에서 함
+		// <input type="file" name="file" > 
+		//  => 이름일치 MultipartFile file
+		System.out.println("BoardController fwritePro()");
+		// 첨부파일 있으면 (enctype="multipart/form-data")
+		// BoardDTO 자동으로 저장 안됨
+		BoardDTO boardDTO=new BoardDTO();
+		boardDTO.setName(request.getParameter("name"));
+		boardDTO.setSubject(request.getParameter("subject"));
+		boardDTO.setContent(request.getParameter("content"));
+		// 파일업로드 프로그램설치 =>pom.xml
+		//첨부파일 
+		// 파일이름 => 랜덤파일이름 => 랜덤문자_파일이름 => 디비저장
+		UUID uuid=UUID.randomUUID();
+		String filename=uuid.toString()+"_"+file.getOriginalFilename();
+		// 원본파일 복사 => 파일위치 / 랜덤문자_파일이름 붙여넣기
+//		FileCopyUtils.copy(원본파일, 복사위치/파일이름);
+		// 외부에 있는 파일 처리 => 예외처리
+		FileCopyUtils.copy(file.getBytes(), new File(uploadPath,filename));
+		
+		// boardDTO file이름 저장
+		boardDTO.setFile(filename);
+		
+		//글쓰기 작업 메서드 호출
+		boardService.insertBoard(boardDTO);
+		// 주소변경 하면서 이동
+		return "redirect:/board/list";
 	}
 
 }
--- SpringProject/src/main/webapp/WEB-INF/views/board/fwriteForm.jsp
+++ SpringProject/src/main/webapp/WEB-INF/views/board/fwriteForm.jsp
@@ -7,19 +7,13 @@
 <title>board/fwriteForm.jsp</title>
 </head>
 <body>
-<%
-String id=(String)session.getAttribute("id");
-if(id==null){
-	response.sendRedirect("MemberLoginForm.me");
-}
-%>
+
 <h1>파일첨부 글쓰기</h1>
-<img src="board/logo.gif">
 <!-- 첨부파일 있으면 꼭 넣기 enctype="multipart/form-data" -->
-<form action="FileBoardWritePro.bo" method="post" enctype="multipart/form-data">
+<form action="${pageContext.request.contextPath}/board/fwritePro" method="post" enctype="multipart/form-data">
 <table border="1">
 <tr><td>글쓴이</td>
-    <td><input type="text" name="name" value="<%=id %>" readonly></td></tr>
+    <td><input type="text" name="name" value="${sessionScope.id }" readonly></td></tr>
 <tr><td>글제목</td>
     <td><input type="text" name="subject" ></td></tr>  
 <tr><td>첨부파일</td>
